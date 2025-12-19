1975031fed5938a920b6d41f05447a9b65c0dcff
madpirate78 <adampitera@gmail.com>
2025-12-19 21:34:39 +0000

    Fix build: convert jest.config.ts to .js to avoid prod type error

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,5 @@
-import type { Config } from 'jest'
-
-const config: Config = {
+/** @type {import('jest').Config} */
+const config = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
@@ -40,4 +39,4 @@ const config: Config = {
   ],
 }
 
-export default config
+module.exports = config
